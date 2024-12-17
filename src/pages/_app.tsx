dfe2b8d47cb25578dda19f8789b7bb6a0d302f0e
Afaq ul Islam <afaqulislam707@gmail.com>
2024-12-17 22:34:03 +0500

    Login Page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,26 @@
 import {
-    ClerkProvider,
-    SignInButton,
-    SignedIn,
-    SignedOut,
-    UserButton,
-  } from '@clerk/nextjs';
-  import '@/styles/globals.css';
-  import type { AppProps } from 'next/app'; // Import AppProps type
-  
-  function App({ Component, pageProps }: AppProps) { // Add type annotations
-    return (
-      <ClerkProvider>
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-        <Component {...pageProps} />
-      </ClerkProvider>
-    );
-  }
-  
-  export default App;
-  
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import "@/app/globals.css";
+import type { AppProps } from "next/app"; // Import AppProps type
+
+function App({ Component, pageProps }: AppProps) {
+  // Add type annotations
+  return (
+    <ClerkProvider>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
+}
+
+export default App;
